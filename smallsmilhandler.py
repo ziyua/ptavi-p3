ca2dfec2e65bac376a1fd09a3228e9d368512585
ziyua <z.y.ma@qq.com>
2014-10-10 20:54:06 +0200

    delete functin print_tags, y add _smil por es private

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -9,7 +9,8 @@ class SmallSMILHandler(ContentHandler, object):
     """
     Obujeto es
     crea un list como:
-    self.smil = [[name of tag, [names of attributes], [value of attributes]], ...]
+    self._smil =
+    [[name of tag, [names of attributes], [value of attributes]], ...]
     """
 
     def _subinit(self):
@@ -26,7 +27,7 @@ class SmallSMILHandler(ContentHandler, object):
     # see me aqui __Init__
     def __init__(self):
         # main smil
-        self.smil = []
+        self._smil = []
         self._subinit()
 
     def _addAttr(self, attrs):
@@ -43,51 +44,48 @@ class SmallSMILHandler(ContentHandler, object):
         """
         list:
         Añade 1: name of tag;
-        2: [names of attributes];
-        3: [value of attributes];
+               2: [names of attributes];
+               3: [value of attributes];
 
-        Añade list a self.smil;
+        Añade list a self._smil;
         """
         listname.append(name)
         listname.append(self.tmpAttrNames)
         listname.append(self.tmpAttr)
-        self.smil.append(listname)
+        self._smil.append(listname)
 
     def startElement(self, name, attrs):
         """
-        1. 'smil' es list total no use para ser un sub-list;
+        1. 'self._smil' es list total; (private important!!)
         eval() name como "self.root_layout" --> list: self.root_layout = []
         clean sub-list, than anade element.
         """
 
-        if name != 'smil':
-            selfname = 'self.' + name
-            selfname = selfname.replace('-', '_')
-            try:
-                selflist = eval(selfname)
-            except AttributeError:
-                pass
-            else:
-                self._subinit()
-                self._addAttr(attrs)
-                self._addNameQAttrs(selflist, name)
+        selfname = 'self.' + name
+        selfname = selfname.replace('-', '_')
+        try:
+            selflist = eval(selfname)
+        except AttributeError:
+            pass
+        else:
+            self._subinit()
+            self._addAttr(attrs)
+            self._addNameQAttrs(selflist, name)
 
     def endElement(self, name):
+        """
+        no usa etiquetas </final> -> pass
+        """
         pass
 
     def characters(self, char):
+        """
+        no tiene contenido entre <div></div> -> pass
+        """
         pass
 
     def get_tags(self):
-        return self.smil
-
-    def print_tags(self):
-        for tags in self.smil:
-            print 'tag name: ' + tags[0]
-            # simple dos tabla, una es nomber de attr, otro es de valor.
-            for num in range(len(tags[1])):
-                print tags[1][num] + ': ' + tags[2][num]
-                print
+        return self._smil
 
 
 def main():
